e98bfbe6305d9c33637e15162bbd0871a9b4f3ce
Jacob Wiedemeier <jacobw-gitkraken@jandjw.com>
2022-04-24 13:07:48 -0500

    Commit attempt #1
    Fully expect all of this to be immediately deprecated / added to gitignore later

--- RunEscape_BaseZero/RunEscape_BaseZero/app.js
+++ RunEscape_BaseZero/RunEscape_BaseZero/app.js
@@ -1,5 +1,34 @@
 'use strict';
+
 const mariadb = require('mariadb');
 
+//test query
+async function main() {
+    try {
+        const conn = await mariadb.createConnection({
+            host: 'd805d2.stackhero-network.com',
+            user: 'root',
+            password: 'OkhxZ62FIP4f5YRJdjC4tjP9E0lvLO2w',
+            database: 'basezero'
+        });
+        // Use Connection to get contacts data
+        var rows = await get_contacts(conn);
 
+        //Print list of contacts
+        for (i = 0, len = rows.length; i < len; i++) {
+            console.log(`${rows[i].Name} ${rows[i].Attack} <${rows[i].FK_ItemID}>`);
+        }
+    } catch (err) {
+        // Manage Errors
+        console.log(err);
+    } finally {
+        // Close Connection
+        if (conn) conn.close();
+    }
+}
+function get_contacts(conn) {
+    return conn.query("SELECT * FROM basezero.Enemy");
+}
+
+main();
 console.log('Hello world');
