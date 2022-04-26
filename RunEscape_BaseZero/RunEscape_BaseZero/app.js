3e1e71b0efd5ce59bf6bd47891c0bb524d35b4ef
zacharyjbaldwin <zacharyjbaldwin@gmail.com>
2022-04-25 23:43:01 -0500

    refactored some code

--- RunEscape_BaseZero/RunEscape_BaseZero/app.js
+++ RunEscape_BaseZero/RunEscape_BaseZero/app.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const app  = express();
-const path = require('path');
-const router = express.Router();
+const morgan = require('morgan');
 
 app.use('/static', express.static('public'));
+app.use(morgan('dev'));
 
-router.get('/', function(req, res){
-    res.sendFile(path.join(__dirname+'/interface.html'));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/interface.html');
+})
+
+app.listen(process.env.port || 3000, () => {
+    console.log("Running on port 3000");
 });
-
-app.use('/', router);
-app.listen(process.env.port || 3000);
-
-console.log("Running on port 3000");
